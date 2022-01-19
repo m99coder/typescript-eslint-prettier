fdf305f5c2d53c13d9b46fb0fb0714ab35e512f0
Marco Lehmann <m99@posteo.de>
2022-01-19 20:24:21 +0100

    add another test case

--- test/text-utils.spec.ts
+++ test/text-utils.spec.ts
@@ -4,4 +4,8 @@ describe('TextUtil', () => {
   it('knows that the length of “hello” is less than 10', () => {
     expect(TextUtils.isLessThanLength('hello', 10)).toBeTruthy()
   })
+
+  it('accepts negative numbers for length parameter', () => {
+    expect(TextUtils.isLessThanLength('hello', -5)).toBeFalsy()
+  })
 })
