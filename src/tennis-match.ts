c733e50d0debaf7eeb53c932b949a41e5eed3353
Marco Lehmann <m99@posteo.de>
2022-01-27 21:38:44 +0100

    add `TennisMatch` exercise

---
+++ src/tennis-match.ts
@@ -0,0 +1,78 @@
+export enum Point {
+  LOVE = 0,
+  FIFTEEN = 15,
+  THIRTY = 30,
+  FORTY = 40,
+  ADVANTAGE = 60,
+}
+
+export class Player {
+  private _id = Math.random()
+  private _points = Point.LOVE
+
+  public get id() {
+    return this._id
+  }
+
+  public get points() {
+    return this._points
+  }
+
+  public set points(points: Point) {
+    this._points = points
+  }
+}
+
+export class TennisMatch {
+  private _players: Player[] = []
+
+  constructor(players: Player[]) {
+    if (players.length !== 2) {
+      throw new Error('Provide exactly two players')
+    }
+
+    this._players = players
+  }
+
+  public get players() {
+    return this._players
+  }
+
+  public pointFor(player: Player) {
+    const opponent = this._players.find((p) => p.id !== player.id)
+    if (!opponent) {
+      throw new Error('Did not find opponent')
+    }
+
+    // advantage can only be reached from a previous deuce,
+    // in case the other player made the point, the score is reset to deuce
+    if (opponent.points === Point.ADVANTAGE) {
+      opponent.points = Point.FORTY
+      return
+    }
+
+    switch (player.points) {
+      case Point.LOVE:
+        player.points = Point.FIFTEEN
+        break
+      case Point.FIFTEEN:
+        player.points = Point.THIRTY
+        break
+      case Point.THIRTY:
+        player.points = Point.FORTY
+        break
+      case Point.FORTY:
+        // there needs to be a difference of two at least to win a game
+        if (opponent.points <= Point.THIRTY) {
+          opponent.points = Point.LOVE
+          player.points = Point.LOVE
+        } else {
+          player.points = Point.ADVANTAGE
+        }
+        break
+      case Point.ADVANTAGE:
+        opponent.points = Point.LOVE
+        player.points = Point.LOVE
+    }
+  }
+}
---
+++ test/tennis-match.spec.ts
@@ -0,0 +1,122 @@
+import { Player, Point, TennisMatch } from '../src/tennis-match'
+
+describe('TennisMatch', () => {
+  let match: TennisMatch
+
+  /**
+   * Serving & receiving:
+   * At the start of the game, the server and the receiver are decided.
+   * In real life, we do a coin toss, but for our game,
+   * you could use merely refer to the players as server and receiver.
+   */
+  describe('Serving & receiving', () => {
+    it('accepts two players', () => {
+      const player1 = new Player()
+      const player2 = new Player()
+      match = new TennisMatch([player1, player2])
+
+      expect(match.players.length).toBe(2)
+    })
+
+    it('throws an error if not providing two players', () => {
+      const player1 = new Player()
+
+      expect(() => new TennisMatch([player1])).toThrowError(
+        'Provide exactly two players'
+      )
+    })
+  })
+
+  /**
+   * Scoring:
+   * In Tennis, there are 4 points.
+   * The first point — which represents zero — is called “love”.
+   * It goes “love”, “15”, “30”, and then “40”.
+   * Whenever the ball isn’t served back, whoever’s side it was last on,
+   * the other player gets a point.
+   */
+  describe('Scoring', () => {
+    let player1: Player
+    let player2: Player
+
+    beforeEach(() => {
+      player1 = new Player()
+      player2 = new Player()
+      match = new TennisMatch([player1, player2])
+    })
+
+    it('start with 0', () => {
+      expect(player1.points).toEqual(Point.LOVE)
+    })
+
+    it('give a point to count 15', () => {
+      match.pointFor(player1)
+      expect(player1.points).toEqual(Point.FIFTEEN)
+    })
+
+    it('give two points to count 30', () => {
+      match.pointFor(player1)
+      match.pointFor(player1)
+      expect(player1.points).toEqual(Point.THIRTY)
+    })
+
+    it('give three points to count 40', () => {
+      match.pointFor(player1)
+      match.pointFor(player1)
+      match.pointFor(player1)
+      expect(player1.points).toEqual(Point.FORTY)
+    })
+  })
+
+  /**
+   * Winning:
+   * In order to win a game, you have to win by two.
+   * That means that if the score is tied 40:40 in a “deuce”
+   * (four points each player), then the next person to score wins an “advantage”.
+   * If the player with the advantage scores again, then the game is over and they win.
+   * However, if the player with the “advantage” loses the next point, it goes back to “Deuce”.
+   */
+  describe('Winning', () => {
+    it('first to 4 points wins a game', () => {
+      const player1 = new Player()
+      const player2 = new Player()
+      match = new TennisMatch([player1, player2])
+
+      Array.from({ length: 4 }, () => match.pointFor(player1))
+      expect(player1.points).toEqual(Point.LOVE)
+    })
+
+    describe('you have to win by two', () => {
+      let player1: Player
+      let player2: Player
+
+      beforeEach(() => {
+        player1 = new Player()
+        player2 = new Player()
+        match = new TennisMatch([player1, player2])
+      })
+
+      it('score for advantage player goes back to deuce, if not a second point follows', () => {
+        // advantage player 2
+        Array.from({ length: 3 }, () => match.pointFor(player1))
+        Array.from({ length: 4 }, () => match.pointFor(player2))
+
+        expect(player1.points).toEqual(Point.FORTY)
+        expect(player2.points).toEqual(Point.ADVANTAGE)
+
+        // point player 1
+        match.pointFor(player1)
+        expect(player1.points).toEqual(Point.FORTY)
+        expect(player2.points).toEqual(Point.FORTY)
+      })
+
+      it('winning 2 points in a row after deuce wins a game', () => {
+        Array.from({ length: 3 }, () => match.pointFor(player1))
+        Array.from({ length: 5 }, () => match.pointFor(player2))
+
+        expect(player1.points).toEqual(Point.LOVE)
+        expect(player2.points).toEqual(Point.LOVE)
+      })
+    })
+  })
+})
